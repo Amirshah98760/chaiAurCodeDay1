18950caa9a1de83ad55b40dde99d40e1e6d344a9
Amirshah98760 <amirshah98760@gmail.com>
2024-07-15 22:59:46 +0500

    Chai aur code day1 challenge

---
+++ script.js
@@ -0,0 +1,92 @@
+// Activity 1:Variable Declaration 
+// Task 1
+var num1 = 17301;
+console.log(num1);
+
+// Task 2
+let str = "Amir shah ";
+console.log(str)
+
+// Activity 2:Constant Declaration 
+// Task 3
+
+const isEligible = true;
+console.log(isEligible);
+
+// Activity 3:Data Types 
+// Task 4
+
+const num2 = 439;
+const str1 = "My name is Amir shah ";
+const isCorrect = false;
+const myObj = 
+{
+    name:'Amir shah',
+    Rollno:439,
+    Section:'G'
+}
+
+const myArr =[1,2,3,4,5];
+
+console.log(typeof num2);
+console.log(typeof str1);
+console.log(typeof isCorrect);
+console.log(typeof myObj);
+console.log(typeof myArr);
+
+
+// Activity 4:Reassigning Variables
+let myName = 'Amir shah';
+console.log(myName);;
+
+myName = 'Latif ur Rehman';
+console.log(myName)
+
+// Activity 5:Understanding const
+
+// Task 6:
+
+const val1 = 439;
+console.log(val1);
+
+// val1 = 347;
+// console.log(val1);
+
+
+// Feature Request Practice 
+
+// 1:Variable types console log 
+const num3 = 439;
+const str2 = "My name is Amir shah ";
+const isFalse = false;
+const myObj1 = 
+{
+    name:'Amir shah',
+    Rollno:439,
+    Section:'G'
+}
+
+const myArr1 =[1,2,3,4,5];
+
+console.log(num3,'The type is  ' +typeof num3);
+console.log(str2,'The type is ' +typeof str2);
+console.log(isFalse, typeof isCorrect);
+console.log(myObj1,typeof myObj1);
+console.log(myArr1,typeof myArr1);
+
+// 2:Reassigning Demo: 
+let myName1 = 'Sajjad Ali';
+
+console.log(myName1);
+
+myName1 = 'Amir shah ';
+console.log(myName1)
+
+
+const myName2 = 'Sajjad Ali';
+
+console.log(myName2);
+
+// myName2 = 'Amir shah ';
+// console.log(myName2);
+
